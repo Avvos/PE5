5fa8af4a8c522569f6efb443e7370d116025204c
camka14 <37488951+camka14@users.noreply.github.com>
2020-02-18 19:13:30 -0700

    Final Update to Rectangle.cpp

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -1,14 +1,20 @@
 #include "Rectangle.h"
 #include <cstdlib>
 
+// Bug Fix: Prevent points to be inserted other than point one bottom left and point 2 top right
 Rectangle::Rectangle(Point p1, Point p2){
-    if(p1.x > p2.x && p1.y > p2.y){
+    if(p1.x > p2.x && p1.y > p2.y){ // checks if the first point is to the top right of the second and flips them
         Point t = p1;
         p1 = p2;
         p2 = t;
-    }else if(p1.x > p2.x && p1.y < p2.y){
+    }else if(p1.x > p2.x && p1.y < p2.y){ // checks if the second point is to the left and above the first and flips their x's 
         Point t1 = {.x = p2.x, .y = p1.y};
         Point t2 = {.x = p1.x, .y = p2.y};
+        p1 = t1;
+        p2 = t2;
+    }else if(p1.x < p2.x && p1.y > p2.y){ // checks if the first point is above and to the right of the second and flips their y's
+        Point t1 = {.x = p1.x, .y = p2.y};
+        Point t2 = {.x = p2.x, .y = p1.y};
         p1 = t1;
         p2 = t2;
     }
@@ -28,15 +34,24 @@ int Rectangle::GetHeight(){
 }
 
 // returns true if this rectangle shares any points with the other one
+// Bug Fix: Added a check if the points don't intersect one onther but the edges still cross each other
 bool Rectangle::Overlaps(Rectangle& other){
-    Point P1 = this->p1_;
-    Point P2 = this->p2_;
-    Point P3 = other.get_p1();
-    Point P4 = other.get_p2();
-
+    Point p1 = this->p1_;
+    Point p2 = this->p2_;
+    Point p3 = other.get_p1();
+    Point p4 = other.get_p2();
+    
+    if(p1.x > p4.x || p3.x > p2.x){ // checks if the rectangles are to the left or right of one another
+        return false;
+    }else if(p1.y > p4.y || p3.y > p2.y){ // checks if the rectangles are above or below one another
+        return false;
+    }else{
+        return true;
+    }
 }
 
 // returns the area of this rectangle
+// Bug Fix: Added an absolute value
 int Rectangle::CalculateArea(){
     int dx = p2_.x - p1_.x;
     int dy = p2_.y - p1_.y;
@@ -54,6 +69,7 @@ void Rectangle::Expand(){
 
 // moves the bottom left coordinate up one and to the right one
 // moves the upper right coordinate down one and to the left one
+// Bug Fix: Added a check for the area before continuing the shrink
 void Rectangle::Shrink(){
     if(this->CalculateArea() == 0){
         return;
